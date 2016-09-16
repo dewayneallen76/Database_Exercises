ff39eed4e5a03bd8efff2fec57587cb4c9c7ad59
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-16 16:21:49 -0500

    made change to data type for sales

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
-DROP IF EXISTS TABLE albums;
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
-	'id' INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	'artist' VARCHAR(255) NOT NULL, 
-	'name' VARCHAR(255) NOT NULL, 
-	'release date' YEAR(4) NULL, 
-	'sales' DOUBLE(2,2) NULL,
-	'genre' VARCHAR(255) NULL,
-	PRIMARY KEY('id')
+	id INT(255) UNSIGNED NOT NULL AUTO_INCREMENT,
+	artist VARCHAR(255) NOT NULL, 
+	name VARCHAR(255) NOT NULL, 
+	release_date YEAR(4) NULL, 
+	sales INT UNSIGNED NULL,
+	genre VARCHAR(255) NULL,
+	PRIMARY KEY(id)
 );
