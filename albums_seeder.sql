6177749b3acd0b26af301470d7a9d769db0205b7
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-16 16:21:03 -0500

    using insert to insert values into sql table

---
+++ albums_seeder.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Micheal Jackson', 'Thriller', 1982, 46.0, 'Pop, rock, R&B'),
+	   ('AC/DC', 'Back in Black', 1980, 26.1, 'Hard Rock'),
+	   ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.2, 'Progressive Rock'),
+	   ('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B, soul, pop'),
+	   ('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard rock, progressive rock'),
+	   ('Eagles', 'Their Greatest Hits', 1976, 32.3, 'Rock, soft rock, folk rock');
