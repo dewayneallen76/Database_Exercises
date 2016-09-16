ea084ae6eec334f52dfe0f61da00d0492f942a45
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-16 15:21:10 -0500

    added values for table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,11 @@
 USE codeup_test_db;
+DROP IF EXISTS TABLE albums;
+CREATE TABLE albums (
+	'id' INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	'artist' VARCHAR(255) NOT NULL, 
+	'name' VARCHAR(255) NOT NULL, 
+	'release date' YEAR(4) NULL, 
+	'sales' DOUBLE(2,2) NULL,
+	'genre' VARCHAR(255) NULL,
+	PRIMARY KEY('id')
+);
