4dbec611740cf5f30643bddb93853764ed29845b
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-19 12:21:33 -0500

    Convert the SELECT statements to DELETE.

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,16 +1,15 @@
 USE codeup_test_db;
-
+-- Convert the SELECT statements to DELETE.
 -- Albums released after 1991
-SELECT 'ALBUMS RELEASE AFTER 1991' as 'Info';
-SELECT name FROM albums 
+DELETE FROM albums 
 WHERE release_date >1991;
 
 -- Albums with the genre "disco"
-SELECT 'ALBUMS WITH GENRE DISCO' as 'Info';
-SELECT name FROM albums
+DELETE FROM albums
 WHERE genre = 'Disco';
 
 -- Albums by "Whitney Houston" (...or maybe an artist of your choice)
-SELECT 'ALBUMS BY WHITNEY HOUSTON' as 'Info';
-SELECT name FROM albums
+DELETE FROM albums
 WHERE artist = 'The Beatles';
+
+SELECT * FROM albums;
