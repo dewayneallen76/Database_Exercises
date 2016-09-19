e680b0954157ed95cf7dbab24b395f7ada589181
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-19 11:57:59 -0500

    changed sales value to int from year

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE albums (
 	id INT(255) UNSIGNED NOT NULL AUTO_INCREMENT,
 	artist VARCHAR(255) NOT NULL, 
 	name VARCHAR(255) NOT NULL, 
-	release_date YEAR(4) NULL, 
+	release_date INT (4) NULL, 
 	sales INT UNSIGNED NULL,
 	genre VARCHAR(255) NULL,
 	PRIMARY KEY(id)
