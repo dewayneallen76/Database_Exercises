6dd101cb7c7b20f5f817aa4340743fcc2792da32
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-20 11:49:01 -0500

    refactored search

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -5,7 +5,7 @@ SELECT DISTINCT title FROM titles;
 SELECT DISTINCT	title FROM titles GROUP BY title;
 
 -- Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY.
-SELECT last_name FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e' GROUP BY last_name;
+SELECT last_name FROM employees WHERE last_name LIKE 'e%e' GROUP BY last_name;
 
 -- Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
 SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
