c84c91c8b5fb093cdf29588ae2f0d470fbb5c622
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-20 15:19:47 -0500

    edited queries to match output for exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -15,8 +15,7 @@ GROUP BY FULLNAME
 ORDER BY NUMBER DESC;
 
 
-SELECT CONCAT(emp_no, '-', first_name, ' ', last_name, birth_date) AS full_name, birth_date AS DOB FROM employees
-ORDER BY full_name 
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB FROM employees
 LIMIT 10;
 
 
