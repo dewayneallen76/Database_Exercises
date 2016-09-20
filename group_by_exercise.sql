2b7f502a177811a81af5544e6286816c9de0b713
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-20 11:47:23 -0500

    changed order by to group by

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -2,7 +2,7 @@
 SELECT DISTINCT title FROM titles;
 
 -- Update the previous query to sort the results alphabetically.
-SELECT DISTINCT	title FROM titles ORDER BY title;
+SELECT DISTINCT	title FROM titles GROUP BY title;
 
 -- Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY.
 SELECT last_name FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e' GROUP BY last_name;
@@ -12,3 +12,4 @@ SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'e%' A
 
 -- Find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY. Your results should be:
 SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
