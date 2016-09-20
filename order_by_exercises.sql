1bd89f91367fa0d5295b9afa4c79e4c135c53114
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-20 10:41:05 -0500

    Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -6,9 +6,12 @@ SELECT * FROM employees WHERE gender = 'M' AND (first_name ='Irena' OR first_nam
 -- Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
 ORDER BY last_name ASC, first_name ASC;
 -- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
-SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE'%e';
+-- Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE'%e'
+ORDER BY emp_no ASC;
 -- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
-SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
+ORDER BY emp_no ASC;
 -- Find ALL employees hired IN the 90s AND born ON Christmas — 362 rows. 
 SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
 -- Find all employees with a 'q' in their last name but not 'qu'— 547 rows.
