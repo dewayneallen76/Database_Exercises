44ac99b2c647c4629547f41d7b7bbf78ccf25810
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-21 15:46:28 -0500

    Find the current salary of all current managers.

--- join_exercises.sql
+++ join_exercises.sql
@@ -27,4 +27,20 @@ WHERE d.dept_name = 'Customer Service'
 	AND t.to_date = '9999-01-01'
 GROUP BY title;
 
+-- Find the current salary of all current managers.
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Name', s.salary AS 'Salary'
+FROM departments AS d 
+JOIN dept_manager AS dm
+	ON d.dept_no = dm.dept_no
+JOIN employees AS e
+	ON dm.emp_no = e.emp_no
+JOIN salaries AS s
+	ON e.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01';
+
+
+
+
+
+
 
