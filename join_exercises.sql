d17e088b9ab92d6772615e694eb3eee727e592db
Dewayne Allen <dewayneallen76@gmail.com>
2016-09-21 16:40:51 -0500

    Bonus Find the names of all current employees, their department name, and their current manager's name.

--- join_exercises.sql
+++ join_exercises.sql
@@ -39,8 +39,18 @@ JOIN salaries AS s
 WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01';
 
 
-
-
+-- Bonus Find the names of all current employees, their department name, and their current manager's name.
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name', d.dept_name AS 'Department Name', CONCAT(dme.first_name, ' ', dme.last_name) AS 'Manager Name'
+FROM employees AS e
+JOIN dept_emp AS de
+	ON e.emp_no = de.emp_no
+JOIN departments AS d
+	ON de.dept_no = d.dept_no 
+JOIN dept_manager AS dm 
+	ON d.dept_no = dm.dept_no
+JOIN employees AS dme
+	ON dm.emp_no = dme.emp_no
+WHERE de.to_date = '9999-01-01' AND dm.to_date = '9999-01-01';
 
 
 
