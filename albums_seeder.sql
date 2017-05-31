526b130f18ec33bac50d3ac6094582e9566643bb
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-31 11:34:56 -0500

    refactored to remove duplicate code, reran album seeder

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -11,362 +11,152 @@ VALUES
 'Thriller',
 '1982',
 46000000,
-'Pop, rock, R&B');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Pop, rock, R&B'),
 ('AC/DC',
 'Back in Black',
 '1980',
 26100000,
-'Hard rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Hard rock'),
 ('Pink Floyd',
 'The Dark Side of the Moon',
 '1973',
 24200000,
-'Progressive rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Progressive rock'),
 ('Whitney Houston / Various artists',
 'The Bodyguard',
 '1992',
 27400000,
-'Soundtrack/R&B, soul, pop');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Soundtrack/R&B, soul, pop'),
 ('Meat Loaf',
 'Bat Out of Hell',
 '1977',
 20600000,
-'Hard rock, progressive rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Hard rock, progressive rock'),
 ('Eagles',
 'Their Greatest Hits (1971–1975)',
 '1976',
 32200000,
-'Rock, soft rock, folk rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Rock, soft rock, folk rock'),
 ('Bee Gees / Various artists',
 'Saturday Night Fever',
 '1977',
 20600000,
-'Disco');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Disco'),
 ('Fleetwood Mac',
 'Rumours',
 '1977',
 27900000,
-'Soft rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Soft rock'),
 ('Shania Twain',
 'Come On Over',
 '1997',
 29600000,
-'Country, pop');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Country, pop'),
 ('Led Zeppelin',
 'Led Zeppelin IV',
 '1971',
 29000000,
-'Hard rock, heavy metal');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Hard rock, heavy metal'),
 ('Michael Jackson',
 'Bad',
 '1987',
 20300000,
-'Pop, funk, rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Pop, funk, rock'),
 ('Alanis Morissette',
 'Jagged Little Pill',
 '1995',
 24800000,
-'Alternative rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Alternative rock'),
 ('Celine Dion',
 'Falling into You',
 '1996',
 20200000,
-'Pop, Soft rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Pop, Soft rock'),
 ('The Beatles',
 'Sgt. Peppers Lonely Hearts Club Band',
 '1967',
 13100000,
-'Rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Rock'),
 ('Eagles',
 'Hotel California',
 '1976',
 21500000,
-'Rock, soft rock, folk rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Rock, soft rock, folk rock'),
 ('Mariah Carey',
 'Music Box',
 '1993',
 19000000,
-'Pop, R&B, Rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Pop, R&B, Rock'),
 ('Michael Jackson',
 'Dangerous',
 '1991',
 17600000,
-'Rock, Funk, Pop');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Rock, Funk, Pop'),
 ('Various artists',
 'Dirty Dancing',
 '1987',
 17900000,
-'Pop, rock, R&B');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Pop, rock, R&B'),
 ('Celine Dion',
 'Lets Talk About Love',
 '1997',
 19300000,
-'Pop, Soft rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Pop, Soft rock'),
 ('The Beatles',
 '1',
 '2000',
 21600000,
-'Rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Rock'),
 ('Adele',
 '21',
 '2011',
 22300000,
-'Pop, soul');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Pop, soul'),
 ('The Beatles',
 'Abbey Road',
 '1969',
 14400000,
-'Rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Rock'),
 ('Bruce Springsteen',
 'Born in the U.S.A.',
 '1984',
 19600000,
-'Rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Rock'),
 ('Dire Straits',
 'Brothers in Arms',
 '1985',
 17700000,
-'Rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Rock'),
 ('James Horner',
 'Titanic: Music from the Motion Picture',
 '1997',
 18100000,
-'Soundtrack');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Soundtrack'),
 ('Madonna',
 'The Immaculate Collection',
 '1990',
 19400000,
-'Pop, Dance');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Pop, Dance'),
 ('Metallica',
 'Metallica',
 '1991',
 19900000,
-'Thrash metal, heavy metal');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Thrash metal, heavy metal'),
 ('Nirvana',
 'Nevermind',
 '1991',
 16700000,
-'Grunge, alternative rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Grunge, alternative rock'),
 ('Pink Floyd',
 'The Wall',
 '1979',
 17600000,
-'Progressive rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Progressive rock'),
 ('Santana',
 'Supernatural',
 '1999',
 20500000,
-'Rock');
-INSERT INTO albums
-(`artist`,
-`name`,
-`release_date`,
-`sales`,
-`genre`)
-VALUES
+'Rock'),
 ('Guns N Roses',
 'Appetite for Destruction',
 '1987',
